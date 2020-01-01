152aebc4f5abe298d84478ed0771cf5eaf9418b3
Bruno <pazosbruno@gmail.com>
2020-01-01 03:37:34 -0300

    added function to calculate time spent in a project

--- time_tracker.py
+++ time_tracker.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import json
+from functools import reduce
 from datetime import datetime, timedelta
 
 def new_timestamp():
@@ -33,31 +34,42 @@ def save(data, path):
     with open(path, "w+") as f:
         json.dump(data, f)
 
-def load(project_name, path):
-    if os.path.exists(path):
-        with open(path, "r") as f:
-            data = json.loads(f.read())
-            project = get_project(project_name, data)
-            if project:
-                project = add_timestamp(project)
-                return update_project(project, data)
-            else:
-                return create_project(project_name, data)
-    else:
-        return create_project(project_name)
+
+def load(path):
+    f = open(path, "r")
+    data = json.loads(f.read())
+    f.close()
+    return data
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("project", help="project name")
     parser.add_argument("-p", "--path", help="Path to the JSON data file", default="data.json")
+    parser.add_argument("-s", "--summary", help="Calculate and display summary of projects in data file", type=bool, default=False)
     args = parser.parse_args()
     project = args.project
     path = args.path
+    summary = args.summary
     if project and path:
-        print(f"working on \'{project}\'")
-        data = load(project, path)
-        save(data, path)
-        print(data)
+        if os.path.exists(path):
+            data = load(path)
+            if not data:
+                data = create_project(project)
+
+            if summary:
+                get_summary(data, project)
+            else:
+                p = get_project(project, data)
+                if p:
+                    p = add_timestamp(p)
+                    data = update_project(p, data)
+                else:
+                    data = create_project(project, data)    
+                
+                save(data, path)
+                print(f"working on \'{project}\'")
+                print(data)
+                return data
 
 def add_timestamp(project):
     last_session = project.get("sessions")[-1]
@@ -67,11 +79,36 @@ def add_timestamp(project):
         last_session.update({"end": new_timestamp()})
     return project
 
-def calculate_subtotal():
-    pass
+def format_date(timestamp):
+    return datetime.strptime(timestamp, "%d/%m/%y - %H:%M:%S")
 
-def calculate_total():
-    pass
+def sum_deltas(deltas):
+    initial = timedelta(days=0, hours=0, minutes=0, seconds=0)
+    return reduce(lambda d1, d2: d1+d2, deltas, initial)
+
+def get_summary(data, project_name):
+    total = calculate_total(data, project_name)
+    #import ipdb; ipdb.set_trace()
+    print(f"Time spent working on project: '{project_name}'")
+    hours = total.seconds // 3600
+    minutes = total.seconds // 60
+    seconds = total.seconds % 60
+    print(f"{total.days} days, {hours} hs, {minutes} min, {seconds} secs.")
+
+def calculate_total(data, project_name):
+    projects = data.get("projects")
+    for i, p in enumerate(projects):
+        if p.get("project_name") == project_name:
+            proj = data.get("projects")[i]
+            deltas = []
+            for s in proj.get("sessions"):
+                start = format_date(s.get("start"))
+                end = format_date(s.get("end"))
+                delta = end - start
+                deltas.append(delta)
+            return sum_deltas(deltas)
+        else:
+            print("Project '{project_name}' was not found in data file")
 
 if __name__ == '__main__':
   main()
